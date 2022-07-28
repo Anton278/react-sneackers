f40ddbaf7714053c999cbfdab46da51246acf231
anton278 <nakonechnyi.an@gmail.com>
2022-07-28 15:37:33 +0300

    refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.scss";
+
 import { HashRouter, Routes, Route } from "react-router-dom";
-import MainPage from "./pages/MainPage";
-import FavoritesPage from "./pages/FavoritesPage";
-import OrdersPage from "./pages/OrdersPage";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
+
+import { MainPage } from "./pages/MainPage";
+import { FavoritesPage } from "./pages/FavoritesPage";
+import { OrdersPage } from "./pages/OrdersPage";
+
 import { selectCart } from "./redux/cart/selectors";
 import { useAppDispatch } from "./redux/store";
 import { getProducts } from "./redux/asyncActions";
