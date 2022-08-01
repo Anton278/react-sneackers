621c466341bcb728ea9a6cf5263df1c8a08b1909
anton278 <nakonechnyi.an@gmail.com>
2022-08-01 15:49:40 +0300

    refactored

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,109 +1,115 @@
 import "./Cart.scss";
-import closeImg from "../../assets/img/close.svg";
-import closeHoverImg from "../../assets/img/close-hover.svg";
-import { useState } from "react";
-import CartItem from "../CartItem";
-import CartWithNotification from "../CartWithNotification";
-import CustomGreenButton from "../CustomGreenButton";
-import { useDispatch, useSelector } from "react-redux";
-import { SET_SHOW_CART, placeOrder } from "../../actions";
 
-interface ISneackers {
-    img: string;
-    name: string;
-    price: number;
-    id: number;
-}
+import { useEffect, useState } from "react";
+import { useAppDispatch } from "../../redux/store";
+import { useSelector } from "react-redux";
 
-const Cart = () => {
-    const [closeIcon, setCloseIcon] = useState(closeImg);
+import { CartItem } from "./CartItem";
+import { Overlay } from "../Overlay";
+import { CustomButton } from "../CustomButton";
+import { CartNotification } from "./CartNotification";
+import { Error } from "../Error";
 
-    const dispatch = useDispatch();
-    const cart = useSelector((state: any) => state.cartReducer.cart);
-    const showIsOrderPlaced = useSelector(
-        (state: any) => state.cartReducer.showIsOrderPlaced
-    );
+import { selectCart, selectTotalPrice } from "../../redux/cart/selectors";
+import { clearCart, setShowCart } from "../../redux/cart/slice";
+import {
+    selectSendOrderStatus,
+    selectSendOrderError,
+} from "../../redux/orders/selectors";
+import { sendOrder } from "../../redux/asyncActions";
 
-    const calcTotalPrice = (): number => {
-        let totalPrice: number = 0;
-        cart.forEach((sneackers: ISneackers) => {
-            totalPrice += sneackers.price;
-        });
-        return totalPrice;
-    };
-    const calcTax = (): number => {
-        return calcTotalPrice() * 0.05;
-    };
+import close from "../../assets/img/close.svg";
+import closeHover from "../../assets/img/close-hover.svg";
+import { ICartItem } from "../../assets/types";
 
-    if (showIsOrderPlaced) {
-        return (
-            <>
-                <div className="overlay"></div>
-                <CartWithNotification type="orderPlaced" />
-            </>
-        );
-    } else if (!cart.length) {
-        return (
-            <>
-                <div className="overlay"></div>
-                <CartWithNotification type="emptyCart" />
-            </>
-        );
-    } else {
-        return (
-            <>
-                <div className="overlay"></div>
-                <div className="cart">
-                    <div className="cart__header">
-                        <span>Корзина</span>
-                        <img
-                            src={closeIcon}
-                            alt="close"
-                            onMouseOver={() => setCloseIcon(closeHoverImg)}
-                            onMouseOut={() => setCloseIcon(closeImg)}
-                            width={32}
-                            height={32}
-                            onClick={() =>
-                                dispatch({
-                                    type: SET_SHOW_CART,
-                                    payload: false,
-                                })
-                            }
-                        />
-                    </div>
-                    <div className="cart__items-wrapp">
-                        {cart.map((sneackers: ISneackers) => (
-                            <CartItem
-                                img={sneackers.img}
-                                name={sneackers.name}
-                                price={sneackers.price}
-                                id={sneackers.id}
-                                key={sneackers.id}
+const Cart: React.FC = () => {
+    const [closeImg, setCloseImg] = useState(close);
+
+    const cart = useSelector(selectCart);
+    const sendOrderStatus = useSelector(selectSendOrderStatus);
+
+    const [showCloseBtn, setShowCloseBtn] = useState<boolean>(() => {
+        if (!cart.length || sendOrderStatus === "success") {
+            return false;
+        } else return true;
+    });
+
+    useEffect(() => {
+        if (cart.length) {
+            setShowCloseBtn(true);
+        } else {
+            setShowCloseBtn(false);
+        }
+        if (sendOrderStatus === "success") {
+            setShowCloseBtn(false);
+        }
+    }, [cart.length, sendOrderStatus]);
+
+    const dispatch = useAppDispatch();
+    const sendOrderError = useSelector(selectSendOrderError);
+    const totalPrice = useSelector(selectTotalPrice);
+
+    return (
+        <>
+            <Overlay></Overlay>
+            <div className="cart">
+                <div className="cart__header">
+                    <h3 className="cart__title">Корзина</h3>
+                    {showCloseBtn && (
+                        <button type="button" className="cart__btn">
+                            <img
+                                className="cart__btn-img"
+                                src={closeImg}
+                                alt="close"
+                                onMouseOver={() => setCloseImg(closeHover)}
+                                onMouseOut={() => setCloseImg(close)}
+                                width={32}
+                                height={32}
+                                onClick={() => dispatch(setShowCart(false))}
                             />
+                        </button>
+                    )}
+                </div>
+                {cart.length ? (
+                    <div className="cart__items-wrapp">
+                        {cart.map((product: ICartItem) => (
+                            <CartItem {...product} key={product.id} />
                         ))}
                     </div>
+                ) : sendOrderStatus === "success" ? (
+                    <CartNotification type="order-placed" />
+                ) : sendOrderStatus === "error" ? (
+                    <Error>{sendOrderError}</Error>
+                ) : (
+                    <CartNotification type="empty-cart" />
+                )}
+                {cart.length ? (
                     <div className="cart__footer">
                         <div className="cart__totals">
                             <div className="cart__total">
                                 <span>Итого:</span>
                                 <div></div>
-                                <b>{calcTotalPrice()} грн.</b>
+                                <b>{totalPrice} грн.</b>
                             </div>
                             <div className="cart__total">
                                 <span>Налог 5%:</span>
                                 <div></div>
-                                <b>{calcTax()} грн.</b>
+                                <b>{totalPrice * 0.05} грн.</b>
                             </div>
                         </div>
-                        <CustomGreenButton
+                        <CustomButton
+                            width="100%"
+                            clickHandler={() => {
+                                dispatch(clearCart());
+                                dispatch(sendOrder(cart));
+                            }}
                             type="place-order"
-                            onClick={() => dispatch(placeOrder(cart))}
-                        />
+                        ></CustomButton>
                     </div>
-                </div>
-            </>
-        );
-    }
+                ) : null}
+            </div>
+        </>
+    );
 };
 
-export default Cart;
+export { Cart };
