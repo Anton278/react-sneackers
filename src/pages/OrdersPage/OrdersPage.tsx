2194520b332bcfe6f6111bff96f2f3a6dc8f4baa
anton278 <nakonechnyi.an@gmail.com>
2022-08-03 14:12:22 +0300

    fix

--- src/pages/OrdersPage/OrdersPage.tsx
+++ src/pages/OrdersPage/OrdersPage.tsx
@@ -1,86 +1,48 @@
-import Header from "../../components/Header";
-import "./OrdersPage.scss";
-import buttonBackImg from "../../assets/img/back.svg";
-import buttonBackHoverImg from "../../assets/img/back-hover.svg";
-import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import SneackersBlock from "../../components/SneackersBlock";
-import EmptyPageNotification from "../../components/EmptyPageNotification";
-import { useDispatch, useSelector } from "react-redux";
-import { getOrders } from "../../actions";
-import Cart from "../../components/Cart";
+import { Header } from "../../components/Header";
+import { Card } from "../../components/Card";
+import { EmptyPageNotification } from "../../components/EmptyPageNotification";
+import { Cart } from "../../components/Cart";
+import { Wrapp } from "../../components/Wrapp";
+import { ProductsWrapp } from "../../components/ProductsWrapp";
+import { PageTitle } from "../../components/PageTitle";
 
-interface ISneackers {
-    name: string;
-    img: string;
-    price: number;
-    id: number;
-}
-interface IOrder {
-    order: Array<ISneackers>;
-    id: string;
-}
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { selectOrders } from "../../redux/orders/selectors";
+import { selectShowCart } from "../../redux/cart/selectors";
+import { useAppDispatch } from "../../redux/store";
+import { getOrders } from "../../redux/asyncActions";
+import { ICartItem, IOrder, IProduct } from "../../assets/types";
 
 const OrdersPage = () => {
-    const [buttonBack, setButtonBack] = useState(buttonBackImg);
-    const navigate = useNavigate();
-    const orders = useSelector((state: any) => state.ordersReducer.orders);
-    const showCart = useSelector((state: any) => state.headerReducer.showCart);
-    const dispatch = useDispatch();
+    const orders = useSelector(selectOrders);
+    const showCart = useSelector(selectShowCart);
+    const dispatch = useAppDispatch();
 
     useEffect(() => {
         dispatch(getOrders());
     }, []);
 
-    if (!orders.length) {
-        return (
-            <div className="wrapp">
-                <Header />
-                {showCart && <Cart />}
+    return (
+        <Wrapp>
+            <Header />
+            {showCart && <Cart />}
+            {orders.length ? (
+                <>
+                    <PageTitle>Мои покупки</PageTitle>
+                    <ProductsWrapp>
+                        {orders.map((order: IOrder) =>
+                            order.products.map((product: ICartItem) => (
+                                <Card {...product} key={product.id} />
+                            ))
+                        )}
+                    </ProductsWrapp>
+                </>
+            ) : (
                 <EmptyPageNotification page="orders" />
-            </div>
-        );
-    } else {
-        return (
-            <div className="wrapp">
-                <Header />
-                {showCart && <Cart />}
-                <main>
-                    <div className="orders-page">
-                        <div className="orders-page__header">
-                            <img
-                                src={buttonBack}
-                                alt="button-back"
-                                onMouseOver={() => {
-                                    setButtonBack(buttonBackHoverImg);
-                                }}
-                                onMouseOut={() => {
-                                    setButtonBack(buttonBackImg);
-                                }}
-                                onClick={() => {
-                                    navigate("/");
-                                }}
-                            />
-                            <span>Мои покупки</span>
-                        </div>
-                    </div>
-                    <div className="sneackers-block-wrapp">
-                        {orders.map((order: IOrder) => {
-                            return order.order.map((sneackers: ISneackers) => (
-                                <SneackersBlock
-                                    name={sneackers.name}
-                                    id={sneackers.id}
-                                    price={sneackers.price}
-                                    img={sneackers.img}
-                                    key={sneackers.id}
-                                />
-                            ));
-                        })}
-                    </div>
-                </main>
-            </div>
-        );
-    }
+            )}
+        </Wrapp>
+    );
 };
 
-export default OrdersPage;
+export { OrdersPage };
